53fb06e09d5533c45c8b8863e62cd80ae937c8cb
Jotaoesee <jadiazm1995@gmail.com>
2025-02-18 10:12:20 +0100

    termino los servidores y termino de ajustar la parte visual

--- flutter_client/lib/main.dart
+++ flutter_client/lib/main.dart
@@ -1,147 +1,203 @@
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final channel = WebSocketChannel.connect(
-    Uri.parse('ws://localhost:8080'),
-  );
-
-  final TextEditingController _controller = TextEditingController();
-  final List<String> _messages =
-      []; // Lista para almacenar solo los mensajes enviados
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Chat WebSocket',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.deepPurpleAccent,
-          elevation: 5,
+        primaryColor: Colors.deepPurpleAccent,
+        scaffoldBackgroundColor: Colors.black87,
+        appBarTheme: AppBarTheme(color: Colors.deepPurpleAccent),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
         ),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepPurple, Colors.purpleAccent],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Selecciona una opción")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WebSocketScreen()));
+              },
+              child: Text("Conectar a WebSocket",
+                  style: TextStyle(color: Colors.white)),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: _messages.length,
-                    itemBuilder: (context, index) {
-                      return Align(
-                        alignment: Alignment.centerRight,
-                        child: Container(
-                          padding: const EdgeInsets.all(12),
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 5, horizontal: 10),
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.9),
-                            borderRadius: BorderRadius.circular(15),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black26,
-                                blurRadius: 5,
-                                spreadRadius: 1,
-                              ),
-                            ],
-                          ),
-                          child: Text(
-                            _messages[index],
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.deepPurple,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(height: 10),
-                _buildMessageInput(),
-              ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WebRTCScreen()));
+              },
+              child:
+                  Text("Iniciar WebRTC", style: TextStyle(color: Colors.white)),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
+}
 
-  Widget _buildMessageInput() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.9),
-        borderRadius: BorderRadius.circular(30),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 5,
-            spreadRadius: 1,
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  hintText: 'Escribe tu mensaje...',
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-          ),
-          IconButton(
-            onPressed: _sendMessage,
-            icon: const Icon(Icons.send, color: Colors.deepPurpleAccent),
-          ),
-        ],
-      ),
-    );
-  }
+class WebSocketScreen extends StatefulWidget {
+  @override
+  _WebSocketScreenState createState() => _WebSocketScreenState();
+}
 
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      channel.sink.add(_controller.text); // Enviar mensaje
+class _WebSocketScreenState extends State<WebSocketScreen> {
+  final WebSocketChannel channel =
+      WebSocketChannel.connect(Uri.parse('ws://localhost:8080'));
+  final TextEditingController _messageController = TextEditingController();
+  final List<String> _messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    channel.stream.listen((message) {
       setState(() {
-        _messages.add(_controller.text); // Agregar solo los mensajes enviados
+        _messages.add("Recibido: $message");
       });
-      _controller.clear(); // Limpiar campo
-    }
+    });
   }
 
   @override
   void dispose() {
     channel.sink.close();
+    _messageController.dispose();
     super.dispose();
+  }
+
+  void _sendMessage() {
+    if (_messageController.text.isNotEmpty) {
+      channel.sink.add(_messageController.text);
+      setState(() {
+        _messages.add("Enviado: ${_messageController.text}");
+      });
+      _messageController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("WebSocket")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: _messageController,
+              decoration: InputDecoration(
+                  labelText: "Escribe un mensaje",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _sendMessage,
+              child: Text("Enviar", style: TextStyle(color: Colors.white)),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) => ListTile(
+                    title: Text(_messages[index],
+                        style: TextStyle(color: Colors.white))),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WebRTCScreen extends StatefulWidget {
+  @override
+  _WebRTCScreenState createState() => _WebRTCScreenState();
+}
+
+class _WebRTCScreenState extends State<WebRTCScreen> {
+  final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
+  final RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
+  RTCPeerConnection? _peerConnection;
+  MediaStream? _localStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeWebRTC();
+  }
+
+  Future<void> _initializeWebRTC() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+    _localStream = await navigator.mediaDevices
+        .getUserMedia({'video': true, 'audio': true});
+    _localRenderer.srcObject = _localStream;
+  }
+
+  @override
+  void dispose() {
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+    _peerConnection?.dispose();
+    _localStream?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("WebRTC")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.deepPurpleAccent, width: 2),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: RTCVideoView(_localRenderer, mirror: true),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text("Vista Remota",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
+                  .copyWith(color: Colors.white)),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.deepPurpleAccent, width: 2),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: RTCVideoView(_remoteRenderer),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
